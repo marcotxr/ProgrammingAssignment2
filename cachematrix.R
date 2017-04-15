e66ecd91a5875553eb73b7b8dafa97de4950a116
marcotxr <marco.mteixeira@gmail.com>
2017-04-15 17:43:27 -0300

    Script completed
    Script completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Marco Teixeira
+## Johns Hopkins and Coursera
+## Data Science Specialization
+## R Programming course - Week 3
+## Programming Assignment 2
 
-## Write a short comment describing this function
+## This function accepts a matrix "x" and creates 3 new function: set(), get() and
+## getinv(). The set() function creates the inverse of the x matrix, the get() 
+## function returns the matrix that was initialy passed by argument to the function
+## and the getinv() function returns the inverse of x matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+   m <<- solve(y) 
+  }
+  get <- function() x
+  getinv <- function() m
+  list(set = set, get = get, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The following function return a matrix that is the inverse of 'x'. If the
+## inverted matrix is already calculted (the m variable) that it returns it from
+## cache, otherwise it calls the set() function from above and return the inverse
+## of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- x$set(data)
+  m
 }
